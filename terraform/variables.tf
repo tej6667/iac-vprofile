070f88795a572ca9fd1e9259eee5788e98ccf9e4
tej6667 <tejtandel14@gmail.com>
2025-08-20 22:38:20 -0400

    updated file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,5 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "vprofile-eks" 
+  default     = "vprofile-eks"
 }
-###
